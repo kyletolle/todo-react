caef71e2c51103313cb4d2919dcfe1f1c8604f8f
Kyle Tolle <kyle@nullsix.com>
2021-02-24 14:25:42 -0700

    Add inital work for Todos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState }  from 'react'
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (event) => {
+    const todoElement = document.getElementById('addTodo')
+    const todoText = todoElement.value;
+
+    const todo = {
+      text: todoText,
+      checked: false,
+    }
+
+    setTodos([todo, ...todos]);
+    todoElement.value = '';
+    event.preventDefault();
+  }
+
+  const todoElements = todos.map((todoItem, todoIndex) => {
+    const todoId = `todoItem${todoIndex}`;
+    const todoKey = `${todoIndex}`;
+
+    const handleChange = (event) => {
+      const isChecked = event.target.value === 'on';
+      const newTodoItem = {
+        ...todoItem,
+        checked: isChecked
+      };
+      const newTodos = [...todos];
+      newTodos[todoIndex] = newTodoItem;
+      setTodos(newTodos);
+    };
+
+    return (
+      <div key={todoKey}>
+        <label>
+          <input type={"checkbox"} id={todoId} checked={todoItem.checked} onChange={handleChange} />
+          {todoItem.text}
+        </label>
+      </div>
+    );
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="App-form" onSubmit={handleAddTodo}>
+          <div>
+            <input type={"text"} id="addTodo" placeholder="Add a Todo"></input>
+          </div>
+          <hr />
+          {todoElements.length > 0 ? todoElements :
+          (
+            <div>Try adding a Todo above!</div>
+          )}
+        </form>
       </header>
     </div>
   );
